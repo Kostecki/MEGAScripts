ad1ea7df64c8a83170dabbdd0a82452bb953f547
Jacob Kostecki <kostecki.jacob@gmail.com>
2018-06-27 22:14:00 +0200

    make script work with arduino nano

--- gatherData/getVoltageFromBattery.py
+++ gatherData/getVoltageFromBattery.py
@@ -1,23 +1,31 @@
-import os
+import serial
+import time
 import json
 import urllib.request
 import base64
 
 #API URL
-apiUrl = "https://putsreq.com/13pmCr7IuFD50z8RzzOz"
+apiUrl = "https://api.mega.re/setvoltage"
 
 #Define final object
-setVoltageData = {}
+Voltage = {}
 
 #Get data from pi-voltmeter
+readings = []
+ser = serial.Serial('/dev/ttyUSB0',9600)
 
+for i in range(0, 600): #600 is about 5 minutes
+  readings.append(ser.readline().rstrip())
+ser.close()
+
+Voltage = str(readings[len(readings)//2])
 
 #Post to API
 auth = '%s:%s' % ('', '') #I'm lazy. Just set login here. User/pass
 base64string = base64.standard_b64encode(auth.encode('utf-8'))
 
 req = urllib.request.Request(apiUrl)
-jsondata = json.dumps(setVoltageData)
+jsondata = json.dumps(Voltage)
 jsondataasbytes = jsondata.encode('utf-8')
 req.add_header('Content-Type', 'application/json; charset=utf-8')
 req.add_header('Content-Length', len(jsondataasbytes))
