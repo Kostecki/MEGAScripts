308c14b0a365aec98d5db85bad955aecaa642971
Jacob Kostecki <kostecki.jacob@gmail.com>
2018-08-15 14:30:05 +0200

    Add check for FW and update

--- lightsController/lightsController.ino
+++ lightsController/lightsController.ino
@@ -12,9 +12,6 @@
 #include <ArduinoOTA.h>
 #include <ESP8266httpUpdate.h>
 
-//Environment
-const int env = 0; //0 = development, 1 = production
-
 //Config
 #include "config.h"
 
@@ -461,21 +458,10 @@ void loop()
   }
 
   client.loop();
-
+  
   ArduinoOTA.handle();
 
-  /*t_httpUpdate_return ret = ESPhttpUpdate.update("192.168.0.2", 80, "/esp/update/arduino.php", "optional current version string here");
-  switch(ret) {
-      case HTTP_UPDATE_FAILED:
-          Serial.println("[update] Update failed.");
-          break;
-      case HTTP_UPDATE_NO_UPDATES:
-          Serial.println("[update] Update no Update.");
-          break;
-      case HTTP_UPDATE_OK:
-          Serial.println("[update] Update ok."); // may not called we reboot the ESP
-          break;
-  }*/
+  checkForUpdates(); //Check if there's new firmware
 
   //EFFECT BPM
   if (effectString == "bpm")
@@ -1103,4 +1089,58 @@ void showleds()
     setColor(0, 0, 0);
     startFade = false;
   }
+}
+
+void checkForUpdates() {
+  String mac = getMAC();
+  String fwURL = String( fwUrlBase ); //url.com/
+  fwURL.concat( mac );                //url.com/mac
+  String fwVersionURL = fwURL;
+  fwVersionURL.concat( ".version" );  //url.com/mac.version
+
+  Serial.println( "Checking for firmware updates." );
+  Serial.print( "MAC address: " );
+  Serial.println( mac );
+  Serial.print( "Firmware version URL: " );
+  Serial.println( fwVersionURL );
+
+  HTTPClient httpClient;
+  httpClient.begin( fwVersionURL );
+  int httpCode = httpClient.GET();
+  if( httpCode == 200 ) {
+    String newFWVersion = httpClient.getString();
+
+    Serial.print( "Current firmware version: " );
+    Serial.println( FW_VERSION );
+    Serial.print( "Available firmware version: " );
+    Serial.println( newFWVersion );
+
+    int newVersion = newFWVersion.toInt();
+
+    if( newVersion > FW_VERSION ) {
+      Serial.println( "Preparing to update" );
+
+      String fwImageURL = fwURL;
+      fwImageURL.concat( ".bin" );
+      t_httpUpdate_return ret = ESPhttpUpdate.update( fwImageURL );
+
+      switch(ret) {
+        case HTTP_UPDATE_FAILED:
+          Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+          break;
+
+        case HTTP_UPDATE_NO_UPDATES:
+          Serial.println("HTTP_UPDATE_NO_UPDATES");
+          break;
+      }
+    }
+    else {
+      Serial.println( "Already on latest version" );
+    }
+  }
+  else {
+    Serial.print( "Firmware version check failed, got HTTP response code " );
+    Serial.println( httpCode );
+  }
+  httpClient.end();
 }
