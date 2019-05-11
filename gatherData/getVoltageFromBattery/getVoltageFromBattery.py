8f3393b32aeebc7e11c2ad7a657cc3dfc0f4a1bc
Jacob Kostecki <kostecki.jacob@gmail.com>
2019-05-12 00:09:39 +0200

    i'm an idiot..

--- gatherData/getVoltageFromBattery/getVoltageFromBattery.py
+++ gatherData/getVoltageFromBattery/getVoltageFromBattery.py
@@ -9,18 +9,18 @@ firebase_admin.initialize_app(cred, {
 })
 
 #Get data from ADC Port 0 (connected to voltage divider)
-adc = Adafruit_ADS1x15.ADS1115()
-GAIN = 2/3
+# adc = Adafruit_ADS1x15.ADS1115()
+# GAIN = 2/3
 
-readings = []
+# readings = []
 
-for _ in range(50): #Get 50 readings from the battery
-  analog_in = adc.read_adc(0, gain=GAIN)
-  voltage_out = (analog_in * 0.0001875)*4.999 #Pretty excessive, but also seems pretty accurate
-  readings.append(voltage_out)
+# for _ in range(50): #Get 50 readings from the battery
+#   analog_in = adc.read_adc(0, gain=GAIN)
+#   voltage_out = (analog_in * 0.0001875)*4.999 #Pretty excessive, but also seems pretty accurate
+#   readings.append(voltage_out)
 
-readings_sorted = sorted(readings)
-voltage = round(readings_sorted[len(readings)//2], 2)
+# readings_sorted = sorted(readings)
+# voltage = round(readings_sorted[len(readings)//2], 2)
 
 # Get current battery
 current_battery = 'ThunderDuck'
@@ -29,6 +29,6 @@ current_battery = 'ThunderDuck'
 ref = db.reference('/voltages')
 ref.push({
     'batteryId': current_battery,
-    'voltage': voltage,
+    'voltage': 12.3,
     'insertTime': {'.sv': 'timestamp'}
 })
